5573d2d89ad44d78dd459a469cdd3e7a3f5f483c
Jason <1lanncontact@gmail.com>
2012-12-31 10:44:04 +0800

    FIXES

--- entities/other.lua
+++ entities/other.lua
@@ -18,7 +18,6 @@
 --  To Do:
 --  - Finish Cookies
 --  - Back/Forward buttons
---  - Fix sleep!
 
 
 --  -------- Variables
@@ -895,10 +894,9 @@ local function updateClient()
 		fs.delete(firewolfLocation)
 		fs.move(updateLocation, firewolfLocation)
 		shell.run(firewolfLocation)
-		return true
+		error()
 	else
 		fs.delete(updateLocation)
-		return false
 	end
 end
 
@@ -2038,9 +2036,8 @@ pages.settings = function(site)
 		centerWrite(string.rep(" ", 43))
 		centerPrint("Firewolf Settings")
 		centerWrite(string.rep(" ", 43))
-		if fs.exists("/.var/settings") then centerPrint("Designed For: NDF-OS")
-		elseif fs.exists("/.bustedOs") then centerPrint("Designed For: BustedOS")
-		else centerPrint("Designed For: " .. serverList[serverID]) end
+		if not(fs.exists("/.var/settings")) then centerPrint("Designed For: " .. serverList[serverID])
+		else centerPrint("Designed For: NDF-OS") end
 		centerPrint(string.rep(" ", 43))
 		print("")
 
@@ -2158,7 +2155,9 @@ pages.update = function(site)
 
 	print("\n")
 	term.setBackgroundColor(colors[theme["bottom-box"]])
-	for i = 1, 3 do centerPrint(string.rep(" ", 43)) end
+	centerPrint(string.rep(" ", 43))
+	centerPrint(string.rep(" ", 43))
+	centerPrint(string.rep(" ", 43))
 
 	local opt = prompt({{"Update", 7, 10}, {"Cancel", 34, 10}}, "horizontal")
 	if opt == "Update" then
@@ -2497,10 +2496,13 @@ local function loadSite(site)
 		-- Setup environment
 		local cbc, ctc = colors.black, colors.white
 		local nenv = {}
-		for k, v in pairs(env) do nenv[k] = v end
+		for k, v in pairs(env) do if type(env[k]) ~= "table" then nenv[k] = v
+		else
+			nenv[k] = {}
+			for i, d in pairs(v) do nenv[k][i] = d end
+		end end
+
 		nenv.term = {}
-		--nenv.os = {}
-		--nenv.shell = {}
 
 		nenv.term.getSize = function()
 			local wid, hei = env.term.getSize()
@@ -2568,10 +2570,6 @@ local function loadSite(site)
 
 		nenv.print = function(...)
 			return env.print(...)
-		end
-
-		nenv.term.isColor = function()
-			return isAdvanced()
 		end
 
 		local oldScroll = term.scroll
@@ -2697,7 +2695,7 @@ local function loadSite(site)
 
 		local queueWebsiteExit = false
 		nenv.os.pullEvent = function(a)
-			if a == "derp" then return true end
+		if a == "derp" then return true end
 			while true do
 				local e, p1, p2, p3, p4, p5 = env.os.pullEventRaw()
 				if e == event_exitWebsite then
@@ -2716,144 +2714,144 @@ local function loadSite(site)
 			end
 		end
 
-		function nenv.read( _sReplaceChar, _tHistory )
-			term.setCursorBlink( true )
+	function nenv.read( _sReplaceChar, _tHistory )
+		term.setCursorBlink( true )
 
-		    local sLine = ""
-			local nHistoryPos = nil
-			local nPos = 0
-		    if _sReplaceChar then
-				_sReplaceChar = string.sub( _sReplaceChar, 1, 1 )
-			end
-			
-			local w, h = term.getSize()
-			local sx, sy = term.getCursorPos()	
+	    local sLine = ""
+		local nHistoryPos = nil
+		local nPos = 0
+	    if _sReplaceChar then
+			_sReplaceChar = string.sub( _sReplaceChar, 1, 1 )
+		end
+		
+		local w, h = term.getSize()
+		local sx, sy = term.getCursorPos()	
 
-			local function ospullEvent(a)
-				if a == "derp" then return true end
-				while true do
-					local e, p1, p2, p3, p4, p5 = os.pullEventRaw()
-					if e == event_exitWebsite then
-						queueWebsiteExit = true
-						env.error(event_exitWebsite)
-					elseif e == "terminate" then
-						env.error()
-					end
-
-					if e ~= event_exitWebsite and e ~= event_redirect and e ~= event_exitApp 
-							and e ~= event_loadWebsite then
-						if a then
-							if e == a then return e, p1, p2, p3, p4, p5 end
-						else return e, p1, p2, p3, p4, p5 end
-					end
-				end
-			end
-			
-			local function redraw( _sCustomReplaceChar )
-				local nScroll = 0
-				if sx + nPos >= w then
-					nScroll = (sx + nPos) - w
-				end
-					
-				term.setCursorPos( sx, sy )
-				local sReplace = _sCustomReplaceChar or _sReplaceChar
-				if sReplace then
-					term.write( string.rep(sReplace, string.len(sLine) - nScroll) )
-				else
-					term.write( string.sub( sLine, nScroll + 1 ) )
-				end
-				term.setCursorPos( sx + nPos - nScroll, sy )
-			end
-			
+		local function ospullEvent(a)
+			if a == "derp" then return true end
 			while true do
-				local sEvent, param = ospullEvent()
-				if sEvent == "char" then
-					sLine = string.sub( sLine, 1, nPos ) .. param .. string.sub( sLine, nPos + 1 )
-					nPos = nPos + 1
-					redraw()
+				local e, p1, p2, p3, p4, p5 = os.pullEventRaw()
+				if e == event_exitWebsite then
+					queueWebsiteExit = true
+					env.error(event_exitWebsite)
+				elseif e == "terminate" then
+					env.error()
+				end
+
+				if e ~= event_exitWebsite and e ~= event_redirect and e ~= event_exitApp 
+						and e ~= event_loadWebsite then
+					if a then
+						if e == a then return e, p1, p2, p3, p4, p5 end
+					else return e, p1, p2, p3, p4, p5 end
+				end
+			end
+		end
+		
+		local function redraw( _sCustomReplaceChar )
+			local nScroll = 0
+			if sx + nPos >= w then
+				nScroll = (sx + nPos) - w
+			end
+				
+			term.setCursorPos( sx, sy )
+			local sReplace = _sCustomReplaceChar or _sReplaceChar
+			if sReplace then
+				term.write( string.rep(sReplace, string.len(sLine) - nScroll) )
+			else
+				term.write( string.sub( sLine, nScroll + 1 ) )
+			end
+			term.setCursorPos( sx + nPos - nScroll, sy )
+		end
+		
+		while true do
+			local sEvent, param = ospullEvent()
+			if sEvent == "char" then
+				sLine = string.sub( sLine, 1, nPos ) .. param .. string.sub( sLine, nPos + 1 )
+				nPos = nPos + 1
+				redraw()
+				
+			elseif sEvent == "key" then
+			    if param == keys.enter then
+					-- Enter
+					break
 					
-				elseif sEvent == "key" then
-				    if param == keys.enter then
-						-- Enter
-						break
-						
-					elseif param == keys.left then
-						-- Left
-						if nPos > 0 then
-							nPos = nPos - 1
-							redraw()
-						end
-						
-					elseif param == keys.right then
-						-- Right				
-						if nPos < string.len(sLine) then
-							nPos = nPos + 1
-							redraw()
-						end
-					
-					elseif param == keys.up or param == keys.down then
-		                -- Up or down
-						if _tHistory then
-							redraw(" ");
-							if param == keys.up then
-								-- Up
-								if nHistoryPos == nil then
-									if #_tHistory > 0 then
-										nHistoryPos = #_tHistory
-									end
-								elseif nHistoryPos > 1 then
-									nHistoryPos = nHistoryPos - 1
-								end
-							else
-								-- Down
-								if nHistoryPos == #_tHistory then
-									nHistoryPos = nil
-								elseif nHistoryPos ~= nil then
-									nHistoryPos = nHistoryPos + 1
-								end						
-							end
-							
-							if nHistoryPos then
-		                    	sLine = _tHistory[nHistoryPos]
-		                    	nPos = string.len( sLine ) 
-		                    else
-								sLine = ""
-								nPos = 0
-							end
-							redraw()
-		                end
-					elseif param == keys.backspace then
-						-- Backspace
-						if nPos > 0 then
-							redraw(" ");
-							sLine = string.sub( sLine, 1, nPos - 1 ) .. string.sub( sLine, nPos + 1 )
-							nPos = nPos - 1					
-							redraw()
-						end
-					elseif param == keys.home then
-						-- Home
-						nPos = 0
-						redraw()		
-					elseif param == keys.delete then
-						if nPos < string.len(sLine) then
-							redraw(" ");
-							sLine = string.sub( sLine, 1, nPos ) .. string.sub( sLine, nPos + 2 )
-							redraw()
-						end
-					elseif param == keys["end"] then
-						-- End
-						nPos = string.len(sLine)
+				elseif param == keys.left then
+					-- Left
+					if nPos > 0 then
+						nPos = nPos - 1
 						redraw()
 					end
+					
+				elseif param == keys.right then
+					-- Right				
+					if nPos < string.len(sLine) then
+						nPos = nPos + 1
+						redraw()
+					end
+				
+				elseif param == keys.up or param == keys.down then
+	                -- Up or down
+					if _tHistory then
+						redraw(" ");
+						if param == keys.up then
+							-- Up
+							if nHistoryPos == nil then
+								if #_tHistory > 0 then
+									nHistoryPos = #_tHistory
+								end
+							elseif nHistoryPos > 1 then
+								nHistoryPos = nHistoryPos - 1
+							end
+						else
+							-- Down
+							if nHistoryPos == #_tHistory then
+								nHistoryPos = nil
+							elseif nHistoryPos ~= nil then
+								nHistoryPos = nHistoryPos + 1
+							end						
+						end
+						
+						if nHistoryPos then
+	                    	sLine = _tHistory[nHistoryPos]
+	                    	nPos = string.len( sLine ) 
+	                    else
+							sLine = ""
+							nPos = 0
+						end
+						redraw()
+	                end
+				elseif param == keys.backspace then
+					-- Backspace
+					if nPos > 0 then
+						redraw(" ");
+						sLine = string.sub( sLine, 1, nPos - 1 ) .. string.sub( sLine, nPos + 1 )
+						nPos = nPos - 1					
+						redraw()
+					end
+				elseif param == keys.home then
+					-- Home
+					nPos = 0
+					redraw()		
+				elseif param == keys.delete then
+					if nPos < string.len(sLine) then
+						redraw(" ");
+						sLine = string.sub( sLine, 1, nPos ) .. string.sub( sLine, nPos + 2 )
+						redraw()
+					end
+				elseif param == keys["end"] then
+					-- End
+					nPos = string.len(sLine)
+					redraw()
 				end
 			end
-			
-			term.setCursorBlink( false )
-			term.setCursorPos( w + 1, sy )
-			print()
-			
-			return sLine
 		end
+		
+		term.setCursorBlink( false )
+		term.setCursorPos( w + 1, sy )
+		print()
+		
+		return sLine
+	end
 
 		-- Run
 		local fn, err = loadfile(cacheLoc)
@@ -3324,7 +3322,7 @@ local function main()
 	-- Update
 	centerWrite(string.rep(" ", 47))
 	centerWrite("Checking For Updates...")
-	if autoupdate == "true" then if updateClient() then return true end end
+	if autoupdate == "true" then updateClient() end
 
 	-- Modem
 	if not(errPages.checkForModem()) then return end
@@ -3332,10 +3330,8 @@ local function main()
 
 	-- Run
 	parallel.waitForAll(websiteMain, addressBarMain, retrieveSearchResults)
-	return false
 end
 
-local skipExit = false
 local function startup()
 	-- HTTP API
 	if not(http) then
@@ -3390,7 +3386,7 @@ local function startup()
 	end
 
 	-- Run
-	local _, err = pcall(function() skipExit = main() end)
+	local _, err = pcall(main)
 	if err ~= nil then
 		term.setTextColor(colors[theme["text-color"]])
 		term.setBackgroundColor(colors[theme["background"]])
@@ -3466,38 +3462,29 @@ if isAdvanced() then
 end
 term.setCursorBlink(false)
 term.clear()
-term.setCursorPos(1, 1)
 
-if (fs.exists("/.var/settings") or fs.exists("/.bustedOs")) and not(skipExit) then
+if not(fs.exists("/.var/settings")) then
+	term.setCursorPos(1, 1)
+	api.centerPrint("Thank You for Using Firewolf " .. version)
+	api.centerPrint("Made by 1lann and GravityScore")
+	term.setCursorPos(1, 3)
+else
 	term.setBackgroundColor(colors[theme["background"]])
 	term.setTextColor(colors[theme["text-color"]])
 	term.clear()
 	term.setCursorPos(1, 5)
 	term.setBackgroundColor(colors[theme["top-box"]])
-	api.centerPrint(string.rep(" ", 43))
-	api.centerWrite(string.rep(" ", 43))
+	print("")
 	api.centerPrint("Thank You for Using Firewolf " .. version)
-	api.centerWrite(string.rep(" ", 43))
 	api.centerPrint("Made by 1lann and GravityScore")
-	api.centerPrint(string.rep(" ", 43))
-	api.centerWrite(string.rep(" ", 43))
+	print("")
 	if isAdvanced() then api.centerPrint("Click to exit...")
 	else api.centerPrint("Press any key to exit...") end
-	api.centerPrint(string.rep(" ", 43))
 
 	while true do
 		local e = os.pullEvent()
 		if e == "mouse_click" or e == "key" then break end
 	end
-
-	term.setTextColor(colors.white)
-	term.setBackgroundColor(colors.black)
-	term.clear()
-	term.setCursorPos(1, 1)
-elseif not(skipExit) then
-	api.centerPrint("Thank You for Using Firewolf " .. version)
-	api.centerPrint("Made by 1lann and GravityScore")
-	term.setCursorPos(1, 3)
 end
 
 -- Close
