5f7c6d345d0ec97d9a9887eb949a924f44d81905
Jason <1lanncontact@gmail.com>
2013-01-03 22:53:18 +0800

    Whoops

--- entities/other.lua
+++ entities/other.lua
@@ -3597,7 +3597,7 @@ local function loadSite(site)
 							debugLog("Success!")
 							redirect("server")
 							break
-						elseif e == "event_loadWebsite" then
+						elseif e == event_loadWebsite then
 							break
 						end
 					end
